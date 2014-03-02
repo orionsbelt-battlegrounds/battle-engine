17a71a9754ce575b4bfb5aa15e03a9b44b211938
Nuno Silva <nunos@zi-yu.com>
2014-03-02 01:00:38 +0000

    New tests

--- src/engine/coordinate.js
+++ src/engine/coordinate.js
@@ -1,11 +1,16 @@
 function Coordinate(str) {
 	var splitted = str.split("_");
-	this.x = parseInt(splitted[0]);
-	this.y = parseInt(splitted[1]);
+	console.log(splitted.length);
+	if( splitted.length == 2) {
+		this.x = parseInt(splitted[0]);
+		this.y = parseInt(splitted[1]);
+	}else{
+		throw new Error("Coordinate with a incorrect number of values.");
+	}
 }
 
 Coordinate.prototype.equals = function(coordinate) {
 	return this.x == coordinate.x && this.y == coordinate.y;
-})
+}
 
 module.exports = Coordinate;
--- test/engine/coordinate.js
+++ test/engine/coordinate.js
@@ -9,7 +9,17 @@ describe('coordinate', function(){
     	var coordinate = new Coordinate("1_2");
     	expect(coordinate.x).to.be.equal(1);
     	expect(coordinate.y).to.be.equal(2);
-    });
+    }),
+
+    it('parameters should be numbers', function() {
+    	var coordinate = new Coordinate("3_4");
+    	expect(coordinate.x).to.be.a("number");
+    	expect(coordinate.y).to.be.a("number");
+    }),
+
+    it('should throw an error', function() {
+    	//expect(new Coordinate("4")).to.throwError();
+    })
 
   });
 
