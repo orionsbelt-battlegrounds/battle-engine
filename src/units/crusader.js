3c5646fbf606d9f8022878f1581b9fa28d9eddc5
Pedro Santos <donbonifacio@gmail.com>
2014-03-05 13:05:15 +0000

    Add crusader and raptor

---
+++ src/units/crusader.js
@@ -0,0 +1,17 @@
+module.exports = {
+  name : "crusader",
+  attack : 2400,
+  defense: 2200,
+  range : 6
+  code : "c",
+  value : 62,
+  type : "mechanic",
+  category: "heavy",
+  displacement : "air",
+  movementType : nil,
+  movementCost : 4,
+  attackMoves : [],
+  posAttackMoves : [],
+  defenseMoves : [],
+  posDefenseMoves : []
+};
---
+++ src/units/raptor.js
@@ -0,0 +1,17 @@
+module.exports = {
+  name : "raptor",
+  attack : 280,
+  defense: 400,
+  range : 2
+  code : "rp",
+  value : 20,
+  type : "mechanic",
+  category: "light",
+  displacement : "air",
+  movementType : nil,
+  movementCost : 1,
+  attackMoves : [],
+  posAttackMoves : [],
+  defenseMoves : [],
+  posDefenseMoves : []
+};
